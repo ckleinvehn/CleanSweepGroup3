c67253374c2439e68bf34bbbb184b901b18f24ab
Jonathan Hense <jonathan.hense@gmail.com>
2021-10-31 18:39:00 -0500

    some dumb sensorSim tests that probably aren't written properly. these are not comprehensive

---
+++ src/test/java/SensorSimulatorTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SensorSimulatorTest {
+
+    @Test
+    void visitedTest() {
+        FloorPlan oneRoom = FloorPlan.oneRoomFloorPlan(3, 3);
+        Location startingLocation = new Location(0, 0);
+        SensorSimulator sensor = new SensorSimulator(oneRoom, startingLocation);
+        CleanSweep cs = new CleanSweep(250.0, 0, sensor, oneRoom.floorLayout.get(0).get(0), oneRoom.floorLayout.get(0).get(0));
+        cs.sensors.visitedCells(oneRoom);
+        cs.sensors.printVisitedLocations();
+
+    }
+
+    @Test
+    void traversableTest() {
+        FloorPlan oneRoom = FloorPlan.oneRoomFloorPlan(3, 3);
+        Location startingLocation = new Location(0, 0);
+        SensorSimulator sensor = new SensorSimulator(oneRoom, startingLocation);
+        CleanSweep cs = new CleanSweep(250.0, 0, sensor, oneRoom.floorLayout.get(0).get(0), oneRoom.floorLayout.get(0).get(0));
+        cs.sensors.getTraversableDirections(startingLocation);
+    }
+
+    @Test
+    void wallTest() {
+        FloorPlan oneRoom = FloorPlan.oneRoomFloorPlan(3, 3);
+        Location startingLocation = new Location(0, 0);
+        SensorSimulator sensor = new SensorSimulator(oneRoom, startingLocation);
+        CleanSweep cs = new CleanSweep(250.0, 0, sensor, oneRoom.floorLayout.get(0).get(0), oneRoom.floorLayout.get(0).get(0));
+        cs.sensors.isWall(Direction.NORTH);
+        Assertions.assertTrue(cs.currentLocation.wallsPresent.contains(Direction.NORTH));
+    }
+
+    @Test
+    void obstacleTest() {
+        FloorPlan oneRoom = FloorPlan.oneRoomFloorPlan(3, 3);
+        Location startingLocation = new Location(0, 0);
+        SensorSimulator sensor = new SensorSimulator(oneRoom, startingLocation);
+        CleanSweep cs = new CleanSweep(250.0, 0, sensor, oneRoom.floorLayout.get(0).get(0), oneRoom.floorLayout.get(0).get(0));
+        cs.sensors.isObstacle(Direction.NORTH);
+        Assertions.assertTrue(cs.currentLocation.wallsPresent.contains(Direction.NORTH));
+        //assumes we always put the charging/startingLoc at 0,0
+    }
+}
